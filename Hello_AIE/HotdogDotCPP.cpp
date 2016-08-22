860b94ee87daf30f25825b42ffe138887b43bb3f
Henry Banks <henry.banks@students.aie.edu.au>
2016-08-22 11:50:24 -0700

    test commit

---
+++ Hello_AIE/HotdogDotCPP.cpp
@@ -0,0 +1,18 @@
+﻿//
+// Name: Henry Banks <henry.banks@students.aie.edu.au>
+//
+
+/*
+
+*/
+
+#include <iostream>
+
+int main()
+{
+	printf("I used to be an adventurer like you, but then I took an arrow to the knee.\n\n(Press enter to kill guard)");
+
+	getchar();
+
+	return 0;
+}
